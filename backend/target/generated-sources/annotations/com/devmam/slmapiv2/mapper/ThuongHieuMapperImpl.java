092086e9a9ffeeb761639de6cd1392a804f6962f
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-11-17 16:02:06 +0700

    pussh

--- backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/ThuongHieuMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/ThuongHieuMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-17T14:45:09+0700",
+    date = "2025-11-17T16:01:30+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
