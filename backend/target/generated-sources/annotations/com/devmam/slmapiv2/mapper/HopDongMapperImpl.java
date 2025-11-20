3da454841f789ef9c146804659a172342138657f
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-11-20 15:08:19 +0700

    push

--- backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/HopDongMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/HopDongMapperImpl.java
@@ -34,7 +34,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-20T14:56:41+0700",
+    date = "2025-11-20T15:07:25+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
