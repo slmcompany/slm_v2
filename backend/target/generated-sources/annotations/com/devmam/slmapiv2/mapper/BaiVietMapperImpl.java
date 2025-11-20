41d63c3044d1b18719f5e1aec86283a4947aa48b
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-11-20 19:53:07 +0700

    push

--- backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/BaiVietMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/BaiVietMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-20T19:41:03+0700",
+    date = "2025-11-20T19:52:24+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
--- backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/VatTuMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/VatTuMapperImpl.java
@@ -24,7 +24,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-20T19:41:02+0700",
+    date = "2025-11-20T19:52:25+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
