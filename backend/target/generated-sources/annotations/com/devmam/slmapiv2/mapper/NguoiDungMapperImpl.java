05be77bf223c58cb0e10f187eb14f3f08a6ad2b9
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-11-20 17:44:36 +0700

    push

--- backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/NguoiDungMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/NguoiDungMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-20T15:07:26+0700",
+    date = "2025-11-20T17:44:10+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
