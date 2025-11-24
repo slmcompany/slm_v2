8066a798f37af5eada7e5aad1768dbccf3b1410f
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-11-24 15:44:22 +0700

    push

--- backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/TronGoiMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devmam/slmapiv2/mapper/TronGoiMapperImpl.java
@@ -35,7 +35,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-24T14:21:53+0700",
+    date = "2025-11-24T15:43:48+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
